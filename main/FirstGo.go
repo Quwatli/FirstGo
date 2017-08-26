07e5130c33b46aedb2152b9a27b72b5d4286399b
Abdul Rahman Quwatli <abmaneastrum@gmail.com>
2017-08-25 21:47:44 -0400

    Adding 2000 UTF character printer with corresponding hexadecimal, binary and decimal values

--- main/FirstGo.go
+++ main/FirstGo.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	println("Hello World From GO!")
+	fmt.Printf("%x %b %d %x \n", 72, 72, 72, 72)
+	println("\nPrinting out the first 200 characters of a UTF encoder")
+
+	for i := 0; i <= 2000; i++ {
+		fmt.Printf("%x %b %d %q \n", i, i, i, i)
+	}
 }
